0996b235adbb4ccb8bb0b0ab352fe342630f7a22
Erik Bremen <strtok@gmail.com>
2025-06-11 14:32:48 -0700

    Fix wasm build

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(marwood_egui::TemplateApp::new(cc)))),
             )
             .await;
 
